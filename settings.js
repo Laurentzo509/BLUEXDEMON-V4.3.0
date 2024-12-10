53a9d463fc45e5d25c097429a2b8b9de86de7dbf
Laurentzo509 <145812982+Laurentzo509@users.noreply.github.com>
2024-12-09 20:03:43 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "50941091397"
  
-global.botNumber = ["2349019529423"]
+global.botNumber = ["50941091397"]
 
 global.anticall = false
 
